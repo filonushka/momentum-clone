1d5c48704c839246502767ed22232eb270c2535b
filonushka <lero4ka.filonova@mail.ru>
2022-10-12 13:17:10 +0300

    feat: add time and date functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("test");
+const time = document.querySelector(".time");
+const date = document.querySelector(".date");
+
+function showTime() {
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString();
+  time.textContent = currentTime;
+  showDate();
+  setTimeout(showTime, 1000);
+}
+showTime();
+
+function showDate() {
+  const newDate = new Date();
+  const options = {
+    month: "long",
+    day: "numeric",
+  };
+  const currentDate = newDate.toLocaleDateString("en-US", options);
+  date.textContent = currentDate;
+}
