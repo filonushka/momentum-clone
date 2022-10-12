616049710a4f0cbd789598bbda44d2e209d245d2
filonushka <lero4ka.filonova@mail.ru>
2022-10-12 16:10:18 +0300

    feat: add greeting functions

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const time = document.querySelector(".time");
 const date = document.querySelector(".date");
+const greeting = document.querySelector(".greeting");
+const name = document.querySelector(".name");
 
 function showTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
   time.textContent = currentTime;
   showDate();
+  showGreeting();
   setTimeout(showTime, 1000);
 }
 showTime();
@@ -19,3 +22,31 @@ function showDate() {
   const currentDate = newDate.toLocaleDateString("en-US", options);
   date.textContent = currentDate;
 }
+
+function showGreeting() {
+  const date = new Date();
+  const hours = date.getHours();
+
+  function gerTimeOfDay() {
+    const arr = ["morning", "afternoon", "evening", "night"];
+    const num = Math.floor(hours / 6);
+    return arr[num - 1];
+  }
+
+  const timeOfDay = gerTimeOfDay();
+  const greetingText = `Good ${timeOfDay}`;
+  greeting.textContent = greetingText + ", ";
+}
+
+function setLocalStorage() {
+  localStorage.setItem("name", name.value);
+}
+
+function getLocalStorage() {
+  if (localStorage.getItem("name")) {
+    name.value = localStorage.getItem("name");
+  }
+}
+
+window.addEventListener("beforeunload", setLocalStorage);
+window.addEventListener("load", getLocalStorage);
