b694fbddeffd78087d2f1bf027762c52c90bb4e8
filonushka <lero4ka.filonova@mail.ru>
2022-10-15 01:07:04 +0300

    feat: add img, func setBg, bckg slider

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const time = document.querySelector(".time");
 const date = document.querySelector(".date");
 const greeting = document.querySelector(".greeting");
 const name = document.querySelector(".name");
+const slideNextBtn = document.querySelector(".slide-next");
+const slidePrevBtn = document.querySelector(".slide-prev");
 
 window.addEventListener("beforeunload", setLocalStorage);
 window.addEventListener("load", getLocalStorage);
-
 function showTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
@@ -33,12 +34,36 @@ function showGreeting() {
   function gerTimeOfDay() {
     const arr = ["morning", "afternoon", "evening", "night"];
     const num = Math.floor(hours / 6);
-    return arr[num - 1];
+    if (num == "0") {
+      return arr[num + 3];
+    } else {
+      return arr[num - 1];
+    }
   }
 
   const timeOfDay = gerTimeOfDay();
   const greetingText = `Good ${timeOfDay}`;
   greeting.textContent = greetingText + ", ";
+
+  // console.log(timeOfDay);
+
+  function setBg() {
+    //  до 6 или до 7???
+    const randomNum = getRandomNum(1, 6);
+    const bgNum = randomNum.toString().padStart(2, "0");
+    document.body.style.backgroundImage = `url("./assets/images/${timeOfDay}/${bgNum}.jpg")`;
+    // console.log(randomNum, timeOfDay, bgNum);
+  }
+
+  slideNextBtn.addEventListener("click", getSlideNext);
+  slidePrevBtn.addEventListener("click", getSlidePrev);
+
+  function getSlidePrev() {
+    setBg();
+  }
+  function getSlideNext() {
+    setBg();
+  }
 }
 
 function setLocalStorage() {
@@ -56,14 +81,3 @@ function getRandomNum(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
-
-const randomNum = getRandomNum(1, 24);
-// console.log(randomNum);
-
-function setBg() {
-  const timeOfDay = Math.floor(new Date().getHours());
-  const bgNum = randomNum.toString().padStart(2, "0");
-  console.log(timeOfDay, bgNum);
-}
-
-setBg();
