a6a0137bea1d248d21e2bcb40813d6fa209fa99c
filonushka <lero4ka.filonova@mail.ru>
2022-10-16 01:51:02 +0300

    feat: add weather func

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const greeting = document.querySelector(".greeting");
 const name = document.querySelector(".name");
 const slideNextBtn = document.querySelector(".slide-next");
 const slidePrevBtn = document.querySelector(".slide-prev");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+const weatherDescription = document.querySelector(".weather-description");
+const city = document.querySelector(".city");
 
 let randomNum = getRandomNum(1, 7);
 console.log("0", randomNum);
@@ -97,3 +101,24 @@ function getRandomNum(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=en&appid=04f828321cbd374ed321a266c0dbd698&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  weatherIcon.className = "weather-icon owf";
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${data.main.temp}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+}
+
+function setCity(e) {
+  if (e.code === "Enter") {
+    getWeather();
+    city.blur();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", getWeather);
+city.addEventListener("keypress", setCity);
