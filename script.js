f9dcf91e78e6566acd510311ff4f09ace07669f2
filonushka <lero4ka.filonova@mail.ru>
2022-10-16 15:36:33 +0300

    feat: add weather func, change structure

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 const time = document.querySelector(".time");
 const date = document.querySelector(".date");
 const greeting = document.querySelector(".greeting");
-const name = document.querySelector(".name");
+const nameDiv = document.querySelector(".name");
 const slideNextBtn = document.querySelector(".slide-next");
 const slidePrevBtn = document.querySelector(".slide-prev");
 const weatherIcon = document.querySelector(".weather-icon");
 const temperature = document.querySelector(".temperature");
 const weatherDescription = document.querySelector(".weather-description");
-const city = document.querySelector(".city");
+const cityInput = document.querySelector(".city");
 
-let randomNum = getRandomNum(1, 7);
-console.log("0", randomNum);
+let bgIndex = getRandomNum(1, 7);
+console.log("0", bgIndex);
 
-window.addEventListener("beforeunload", setLocalStorage);
-window.addEventListener("load", getLocalStorage);
+cityInput.addEventListener("change", storeUserData);
+nameDiv.addEventListener("change", storeUserData);
+nameDiv.addEventListener("focus", handleNameFocus);
+nameDiv.addEventListener("blur", handleNameBlur);
+nameDiv.addEventListener("keydown", handleNameEnter);
+
+restoreUserData();
+handleNameBlur();
+updateWeather();
+
+setInterval(update, 100);
+setBg();
+
+function update() {
+  showTime();
+  showDate();
+  showGreeting();
+}
+
 function showTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
   time.textContent = currentTime;
-  showDate();
-  setTimeout(showTime, 1000);
 }
-showTime();
 
-showGreeting();
 function showDate() {
   const newDate = new Date();
   const options = {
@@ -35,64 +48,50 @@ function showDate() {
 }
 
 function showGreeting() {
-  const date = new Date();
-  const hours = date.getHours();
-
   const timeOfDay = getTimeOfDay();
   const greetingText = `Good ${timeOfDay}`;
   greeting.textContent = greetingText + ", ";
-
-  function getTimeOfDay() {
-    const arr = ["morning", "afternoon", "evening", "night"];
-    const num = Math.floor(hours / 6);
-    if (num == "0") {
-      return arr[num + 3];
-    } else {
-      return arr[num - 1];
-    }
-  }
-
-  function setBg() {
-    const bgNum = randomNum.toString().padStart(2, "0");
-    const timeOfDay = getTimeOfDay();
-    document.body.style.backgroundImage = `url("./assets/images/${timeOfDay}/${bgNum}.jpg")`;
-    // console.log(randomNum, timeOfDay, bgNum);
-  }
-
-  slideNextBtn.addEventListener("click", getSlideNext);
-  slidePrevBtn.addEventListener("click", getSlidePrev);
-  function getSlideNext() {
-    if (randomNum < 7) {
-      setBg();
-      randomNum = randomNum + 1;
-      // console.log("<7", randomNum);
-    } else {
-      randomNum = randomNum - 6;
-      setBg();
-      // console.log("=7", randomNum);
-    }
-  }
-
-  function getSlidePrev() {
-    if (randomNum > 1) {
-      randomNum = randomNum - 1;
-      setBg();
-      // console.log(">1", randomNum);
-    } else {
-      setBg();
-      randomNum = randomNum + 6;
-      // console.log("=1", randomNum);
-    }
-  }
+}
+function getTimeOfDay() {
+  const date = new Date();
+  const hours = date.getHours();
+  const arr = ["night", "morning", "afternoon", "evening"];
+  const num = Math.floor(hours / 6);
+  return arr[num];
 }
 
-function setLocalStorage() {
-  localStorage.setItem("name", name.value);
+function setBg() {
+  const bgNum = bgIndex.toString().padStart(2, "0");
+  const timeOfDay = getTimeOfDay();
+  document.body.style.backgroundImage = `url("./assets/images/${timeOfDay}/${bgNum}.jpg")`;
+  // console.log(randomNum, timeOfDay, bgNum);
 }
 
-function getLocalStorage() {
-  if (localStorage.getItem("name")) {
-    name.value = localStorage.getItem("name");
+slideNextBtn.addEventListener("click", showNextSlide);
+slidePrevBtn.addEventListener("click", showPrevSlide);
+
+function showNextSlide() {
+  bgIndex = (bgIndex % 6) + 1;
+  setBg();
+}
+
+function showPrevSlide() {
+  bgIndex = ((bgIndex + 4) % 6) + 1;
+  setBg();
+}
+function storeUserData() {
+  localStorage.setItem(
+    "userData",
+    JSON.stringify({ name: nameDiv.textContent, city: cityInput.value })
+  );
+}
+
+function restoreUserData() {
+  const userData = localStorage.getItem("userData");
+  if (userData) {
+    const { name, city } = JSON.parse(userData);
+    nameDiv.textContent = name;
+    cityInput.value = city;
   }
 }
 
@@ -102,23 +101,51 @@ function getRandomNum(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-async function getWeather() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=en&appid=04f828321cbd374ed321a266c0dbd698&units=metric`;
-  const res = await fetch(url);
-  const data = await res.json();
+async function updateWeather() {
+  if (!cityInput.value) return;
 
-  weatherIcon.className = "weather-icon owf";
-  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-  temperature.textContent = `${data.main.temp}°C`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&lang=en&appid=04f828321cbd374ed321a266c0dbd698&units=metric`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const iconCode = data.weather[0].icon;
+
+  temperature.textContent = `${Math.round(data.main.temp)}°C`;
   weatherDescription.textContent = data.weather[0].description;
+  weatherIcon.src = `http://openweathermap.org/img/w/${iconCode}.png`;
+  // weatherIcon.src = `./assets/weather-icons/${iconCode}.svg`;
 }
 
-function setCity(e) {
-  if (e.code === "Enter") {
-    getWeather();
-    city.blur();
+function handleCityChange(event) {
+  if (event.code === "Enter") {
+    updateWeather();
+    cityInput.blur();
   }
 }
 
-document.addEventListener("DOMContentLoaded", getWeather);
-city.addEventListener("keypress", setCity);
+function handleNameFocus() {
+  if (nameDiv.classList.contains("empty")) {
+    nameDiv.textContent = "";
+    nameDiv.classList.remove("empty");
+  } else {
+  }
+}
+
+function handleNameBlur() {
+  storeUserData();
+
+  if (!nameDiv.textContent) {
+    nameDiv.classList.add("empty");
+    nameDiv.textContent = "your name";
+  } else {
+    nameDiv.classList.remove("empty");
+  }
+}
+
+function handleNameEnter(e) {
+  if (e.code == "Enter") {
+    nameDiv.blur();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", updateWeather);
+cityInput.addEventListener("keydown", handleCityChange);
