e7f4eb2ec0ded6da77247a9deba96b42e9e438f7
filonushka <lero4ka.filonova@mail.ru>
2022-10-20 22:57:40 +0300

    feat: add audioList

---
+++ audio-list.js
@@ -0,0 +1,29 @@
+const playList = [
+  {
+    author: "Chaka Knan",
+    song: "Like Sugar",
+    src: "./assets/audio/chaka_khan-like_sugar.mp3",
+  },
+  {
+    author: "Hans Zimmer",
+    song: "Cornfield Chase",
+    src: "./assets/audio/hans_zimmer_-_Cornfield_Chase.mp3",
+  },
+  {
+    author: "James Blake",
+    song: "Say What you will",
+    src: "./assets/audio/james_blake_-_Say_What_You_Will.mp3",
+  },
+  {
+    author: "Metronomy",
+    song: "The Look",
+    src: "./assets/audio/metronomy_-_The_Look.mp3",
+  },
+  {
+    author: "Yann Tiersen",
+    song: "I've Never Been There",
+    src: "./assets/audio/yann_Tiersen_-_Ive_Never_Been_There.mp3",
+  },
+];
+
+export default playList;
--- script.js
+++ script.js
@@ -12,7 +12,20 @@ const quote = document.querySelector(".quote");
 const quoteAuthor = document.querySelector(".quote-author");
 const changeQuoteBtn = document.querySelector(".change-quote__button");
 const playBtn = document.querySelector(".play");
+const playPrevBtn = document.querySelector(".play-prev");
+const playNextBtn = document.querySelector(".play-next");
+const audio = document.querySelector("audio");
+const progressContainer = document.querySelector(".progress-container");
+const progress = document.querySelector(".progress");
+const playListContainer = document.querySelector(".play-list");
+const audioTitle = document.querySelector(".audio-title");
+import playList from "./audio-list.js";
+
 let bgIndex = getRandomNum(1, 7);
+let isPlay = false;
+let audioNum = 0;
+audioTitle.textContent = `${playList[0].author} - ${playList[0].song}`;
+audio.src = `${playList[0].src}`;
 
 slideNextBtn.addEventListener("click", showNextSlide);
 slidePrevBtn.addEventListener("click", showPrevSlide);
@@ -24,6 +37,10 @@ nameDiv.addEventListener("keydown", handleNameEnter);
 document.addEventListener("DOMContentLoaded", updateWeather);
 cityInput.addEventListener("keydown", handleCityChange);
 changeQuoteBtn.addEventListener("click", changeQuote);
+playBtn.addEventListener("click", togglePLayBtn);
+playBtn.addEventListener("click", playAudio);
+playPrevBtn.addEventListener("click", playPrevAudio);
+playNextBtn.addEventListener("click", playNextAudio);
 
 restoreUserData();
 handleNameBlur();
@@ -72,7 +89,6 @@ function setBg() {
   const bgNum = bgIndex.toString().padStart(2, "0");
   const timeOfDay = getTimeOfDay();
   document.body.style.backgroundImage = `url("./assets/images/${timeOfDay}/${bgNum}.jpg")`;
-  // console.log(randomNum, timeOfDay, bgNum);
 }
 
 function showNextSlide() {
@@ -117,7 +133,6 @@ async function updateWeather() {
   temperature.textContent = `${Math.round(data.main.temp)}°C`;
   weatherDescription.textContent = data.weather[0].description;
   weatherIcon.src = `http://openweathermap.org/img/w/${iconCode}.png`;
-  // weatherIcon.src = `./assets/weather-icons/${iconCode}.svg`;
 }
 
 function handleCityChange(event) {
@@ -170,4 +185,55 @@ function togglePLayBtn() {
   playBtn.classList.toggle("pause");
 }
 
-playBtn.addEventListener("click", togglePLayBtn);
+function playPrevAudio() {
+  isPlay = false;
+  console.log("play prev");
+  if (audioNum > 0) {
+    audioNum--;
+    playAudio();
+    console.log(audioNum);
+  } else {
+    audioNum = 4;
+    playAudio();
+    console.log(audioNum);
+  }
+}
+
+function playNextAudio() {
+  console.log("play next");
+  isPlay = false;
+  if (audioNum < 4) {
+    audioNum++;
+    playAudio();
+  } else {
+    audioNum = 0;
+    playAudio();
+    console.log(audioNum);
+  }
+}
+
+function playAudio() {
+  if (!isPlay) {
+    isPlay = true;
+    audioTitle.textContent = `${playList[audioNum].author} - ${playList[audioNum].song}`;
+    audio.src = `${playList[audioNum].src}`;
+    console.log(audioNum, audio.src);
+    audio.play();
+    console.log(isPlay, "play");
+  } else {
+    audio.pause();
+    isPlay = false;
+    audioTitle.textContent = `${playList[audioNum].author} - ${playList[audioNum].song}`;
+    console.log(isPlay, "pause");
+  }
+}
+
+for (let i = 0; i < 5; i++) {
+  function createAudioList() {
+    const li = document.createElement("li");
+    li.classList.add("audio-item");
+    li.textContent = `${playList[i].author} - ${playList[i].song}`;
+    playListContainer.append(li);
+  }
+  createAudioList();
+}
